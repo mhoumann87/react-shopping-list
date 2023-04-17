33b38cd0ef554303aec4bdffe300f5e42459cec3
Michael Houmann <mhoumann87@gmail.com>
2023-04-17 15:08:36 +0200

    Add useEffect for fetching data from server

--- src/App.js
+++ src/App.js
@@ -6,16 +6,32 @@ import AddItem from './components/AddItem.jsx';
 import SearchItem from './components/SearchItem.jsx';
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('shoppinglist')) || []
-  );
+  const API_URL = 'http://localhost:3500/items';
 
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem('shoppinglist', JSON.stringify(items));
-  }, [items]);
+    // Get the data from the "data" base
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchItems();
+  }, []);
 
   const addItem = item => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -62,13 +78,19 @@ function App() {
           handleSubmit={handleSubmit}
         />
         <SearchItem search={search} setSearch={setSearch} />
-        <Content
-          items={items.filter(item =>
-            item.item.toLowerCase().includes(search.toLowerCase())
-          )}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />
+        {isLoading && <div className='loader'></div>}
+        {fetchError && !isLoading && (
+          <p className='err-msg'>{`${fetchError}`}</p>
+        )}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter(item =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        )}
       </main>
       <Footer length={items.length} />
     </>
